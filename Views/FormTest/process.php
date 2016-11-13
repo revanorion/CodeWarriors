1400cbfb175b3779228b070c0ae0c301c5e0e9ca
Kyle Bowman <Kylebowman99@gmail.com>
2016-11-12 23:57:46 -0500

    PHP test script

--- Views/FormTest/process.php
+++ Views/FormTest/process.php
@@ -2,6 +2,5 @@
 header("Content-Type: text/javascript; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, PUT');
-$data = $_POST['data']
-json_decode($data);
+var_dump($_POST);
 ?>
