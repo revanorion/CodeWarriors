9472beca420d3e4fec71a272e5ee244c62ccae8c
Kyle Bowman <Kylebowman99@gmail.com>
2016-11-13 01:25:06 -0500

    Fix minor issue

--- Views/FormTest/process.php
+++ Views/FormTest/process.php
@@ -1,14 +1,17 @@
 <?php
-// $_POST is the serialized form data
 header("Content-Type: text/javascript; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, PUT');
+
 // dump all the data from $_POST
 var_dump($_POST);
+
 // store specific $_POST attribute into variable
 $lole = $_POST["name"];
+
 // display the data
-echo $lole;
+echo $lole, PHP_EOL;
+
 // another way to display the data
 echo $_POST["name"];
 ?>
