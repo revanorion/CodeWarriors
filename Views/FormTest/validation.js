9f72f1ef6418b26951dc6a9f3732e94340eb2971
Kyle Bowman <Kylebowman99@gmail.com>
2016-11-13 00:49:38 -0500

    Separate javascript from html, update process.php

---
+++ Views/FormTest/validation.js
@@ -0,0 +1,31 @@
+$(function() {
+    //validate form fields based on rules
+    $("#formtest").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: "required",
+            pnumber: "required"
+        },
+        messages: {
+            email:{
+                required: "Please enter an email address.",
+                email: "Please enter a <em>valid</em> email address"
+            }
+        },
+        //submitHandler is envoked when validate passes
+        submitHandler : function(form){
+            $.ajax({
+            url: './process.php',
+            type: 'POST',
+            data: $(form).serialize(),
+            dataType: 'json',
+            success: function(response) {
+                alert('Response: ' + response);
+            }            
+            });
+        }
+    });
+});
