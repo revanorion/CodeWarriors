57cdf286469fd7407a82e5981ddd9fd49e47e932
Kyle Bowman <Kylebowman99@gmail.com>
2016-11-23 13:27:04 -0500

    Added validation to check if fau.edu email

--- Views/FormTest/validation.js
+++ Views/FormTest/validation.js
@@ -1,10 +1,25 @@
 $(function() {
+    // Adds function to check whether email is an fau.edu email
+    $.validator.addMethod('customemail', function(value, element) {
+        var allowedDomains = [ 'fau.edu' ];
+        var str = value;
+        domain = str.split('@');
+
+        if ($.inArray(domain[1], allowedDomains) !== -1) {
+            //acceptable
+            return true;
+        }else{
+            //not acceptable
+            return false;
+        }
+    }),
     //validate form fields based on rules
     $("#formtest").validate({
         rules: {
             email: {
                 required: true,
-                email: true
+                email: true,
+                customemail: true
             },
             password: "required",
             pnumber: "required"
@@ -12,7 +27,8 @@ $(function() {
         messages: {
             email:{
                 required: "Please enter an email address.",
-                email: "Please enter a <em>valid</em> email address"
+                email: "Please enter a <em>valid</em> email address",
+                customemail: "Please use a valid fau.edu email address"
             }
         },
         //submitHandler is invoked when validate passes
