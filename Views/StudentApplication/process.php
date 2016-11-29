e51c4908eaf27a273d8d1ed7379471217c9c0b18
Kyle Bowman <Kylebowman99@gmail.com>
2016-11-29 01:56:52 -0500

    Ajax now properly sends json to process.php

--- Views/StudentApplication/process.php
+++ Views/StudentApplication/process.php
@@ -7,10 +7,13 @@ header('Access-Control-Allow-Methods: GET, POST, PUT');
 var_dump($_POST);
 
 // store specific $_POST attribute into variable
-$lole = $_POST["fistname"];
+$lole = $_POST["firstname"];
 
+echo 'This is my first name', PHP_EOL;
 // display the data
-echo $lole, PHP_EOL;
+echo $lole, PHP_EOL, PHP_EOL;
+
+echo 'This is my last name', PHP_EOL;
 
 // another way to display the data
 echo $_POST["lastname"];
