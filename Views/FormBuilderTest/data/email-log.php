5ce859700199705837f13b616e734d1fe6cca0a8
Kyle Bowman <Kylebowman99@gmail.com>
2016-12-03 01:43:53 -0500

    jQuery Form Builder test (use formtemplate.html). IT'S BEAUTIFUL!

---
+++ Views/FormBuilderTest/data/email-log.php
@@ -0,0 +1,10 @@
+<?php exit(); /* For security reason. To avoid public user downloading below data! */?>
+
+
+----------------------------------------------------------------
+2016-12-03 07:32:00
+Email Sent: Failed (You must provide at least one recipient email address.)
+
+
+----------------------------------------------------------------
+
